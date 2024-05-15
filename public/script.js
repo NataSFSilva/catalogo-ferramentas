d815b357f9a2f4e0098bda3316436d1b48998fe2
NataSFSilva <nata.silva@dock.tech>
2024-05-15 15:40:03 -0300

    Add trilha SOS + add caminhos de logs e metricas

--- public/script.js
+++ public/script.js
@@ -5,6 +5,30 @@ class Ferramenta {
     }
 }
 
-function mudarQuestoes() {
-    
+function devAmbient() {
+    document.getElementById('sos').style.display = "flex"
+}
+
+function documentacao(acesso) {
+    if (acesso) {
+        document.getElementById('pilarObs').style.display = "flex"
+    } else {
+        document.getElementById('acesso').style.display = "flex"
+        document.getElementById('pilarObs').style.display = "flex"
+    }
+}
+
+function caminhoPilar(pilar) {
+    if (pilar == "metrics") {
+        var metricas = document.getElementsByClassName('metrics')
+
+        console.log(metricas)
+        console.log(metricas.length)
+        console.log(metricas[0])
+        for (let i = 0; i < metricas.length; i++) {
+            metricas[i].style.display = "flex"
+        }
+    } else if (pilar == "logs") {
+        var metricas = document.getElementsByClassName
+    }
 }
