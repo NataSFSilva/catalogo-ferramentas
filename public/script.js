50a87f42e4cc44b4b03a8809a8fc92dfc207c4c3
NataSFSilva <nata.ssilva@sptech.school>
2024-05-29 10:30:18 -0300

    Add questões de logs e métricas automatizadas

--- public/script.js
+++ public/script.js
@@ -5,20 +5,142 @@ class Formulario {
     }
 }
 
-function loadQuestions() {
-    sessionStorage.setItem("questoes", 5)
+function loadQuestions(qtdQuestoes) {
+    sessionStorage.setItem("questoes", qtdQuestoes)
 }
 
-const matrics = [
+const metrics = [
     {
-        question: "",
+        question: "Qual provedor a aplicação utiliza?",
         answers: [
             {
-                answer: "",
-                type: ""
+                text: "AWS",
+                type: "btn"
+            },
+            {
+                text: "GCP",
+                type: "btn"
+            },
+            {
+                text: "Azure",
+                type: "btn"
+            },
+            {
+                text: "Outros...",
+                type: "inp"
             }
-        ],
+        ]
     },
+    {
+        question: "Tecnologia utilizada? (Java, GO, Python, php, etc.)",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "Qual infraestrutura a aplicação utiliza? Ex: Kubernetes, Docker, VirtualMachine, Lambda...",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "A aplicação possui alguma dependência de pacotes de ferramentas de observabilidade?",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "Caso a infraestrutura seja conteinerizada qual o máximo e mínimo de escalonamento de pods e nodes?",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "Qual provedor a aplicação utiliza?",
+        answers: [
+            {
+                text: "CPU",
+                type: "btn"
+            },
+            {
+                text: "Disco",
+                type: "btn"
+            },
+            {
+                text: "Memória",
+                type: "btn"
+            },
+            {
+                text: "Network",
+                type: "btn"
+            },
+            {
+                text: "Data Base",
+                type: "btn"
+            },
+        ]
+    },
+    {
+        question: "Qual a quantidade de chamadas a aplicação recebe por dia? (valor médio)",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "Caso necessário. Quais métricas de APM serão utilizadas? Ex: Resource",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "A aplicação exige auditoria 100% das métricas para validação? Ex: Métrica transacional Pix, precisa devido a criticidade.",
+        answers: [
+            {
+                text: "Sim",
+                type: "btn"
+            },
+            {
+                text: "Sim",
+                type: "btn"
+            }
+        ]
+    },
+    {
+        question: "Qual o impacto da aplicação caso ela fique indisponível?",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    },
+    {
+        question: "Informar qual o time responsável pela aplicação (conhecimento das regras de negócio) para desenvolvimento dos objetos monitoria (dashboard, alert e report)",
+        answers: [
+            {
+                text: "Texto de resposta curta",
+                type: "inp"
+            }
+        ]
+    }
 ]
 
 const logs = [
@@ -30,7 +152,7 @@ const logs = [
                 type: "inp"
 
             },
-        ],
+        ]
     },
     {
         question: "Qual será o tempo de retenção?",
@@ -51,7 +173,7 @@ const logs = [
                 text: "Outros...",
                 type: "inp"
             }
-        ],
+        ]
     },
 
     {
@@ -66,7 +188,7 @@ const logs = [
                 type: "btn"
 
             }
-        ],
+        ]
     },
     {
         question: "Os logs são estruturados em Json?",
@@ -80,7 +202,7 @@ const logs = [
                 type: "inp"
 
             }
-        ],
+        ]
     },
 
     {
@@ -103,7 +225,7 @@ const logs = [
                 type: "btn"
 
             }
-        ],
+        ]
     },
     {
         question: "É esperado o crescimento progressivo da volumetria dos logs?",
@@ -117,7 +239,7 @@ const logs = [
                 type: "btn"
             }
 
-        ],
+        ]
     },
     {
         question: "Favor disponibilizar um log para análise dos parâmetros:",
@@ -127,7 +249,7 @@ const logs = [
                 type: "inp"
 
             }
-        ],
+        ]
     },
 
     {
@@ -137,7 +259,7 @@ const logs = [
                 text: "Outros...",
                 type: "inp"
             }
-        ],
+        ]
     },
     {
         question: "Qual o tipo de integração que será utilizada para coleta de log?",
@@ -162,7 +284,7 @@ const logs = [
                 text: "Outros...",
                 type: "inp"
             }
-        ],
+        ]
     },
     {
         question: 'Caso o envio seja via HTTP, é possível o "acknowledgement" na solução do envio? Confirmação do recebimento do evento por parte da ferramenta de observabilidade. (obs: aplicações que utilizam o kafka para o envio precisam da opção habilitada)',
@@ -189,7 +311,7 @@ const logs = [
                 text: "Outros...",
                 type: "inp"
             }
-        ],
+        ]
     },
     {
         question: "Qual o impacto da aplicação caso ela fique indisponível?",
@@ -198,7 +320,7 @@ const logs = [
                 text: "Outros...",
                 type: "inp"
             }
-        ],
+        ]
     },
     {
         question: "Informar qual o time responsável pela aplicação (conhecimento das regras de negócio) para desenvolvimento dos objetos de monitoria (dashboard, alert e report)",
@@ -207,27 +329,51 @@ const logs = [
                 text: "Outros...",
                 type: "inp"
             }
-        ],
+        ]
     }
 ]
 
-/*
-<div class="container logs" style="display: none;">
-</div>
-    <div class="box">
-      <h2>17. As métricas de infraestrutura da aplicação estão sendo monitoradas? Favor informar qual aplicação está
-        sendo utilizada.</h2>
-      <button class="btn">Não</button>  
-      <input type="text" class="inp" placeholder="Outros...">
-    </div>
-*/
+function showMetricsQuestions() {
+    var qtdQuestoes = sessionStorage.getItem("questoes")
+
+    metrics.forEach(question => {
+        qtdQuestoes++
+
+        var container = document.createElement("div")
+        container.classList.add("container", "metrics")
+        
+        var box = document.createElement("div")
+        box.classList.add("box")
+        container.appendChild(box)
+
+        var title = document.createElement("h2")
+        title.textContent = `${qtdQuestoes}. ${question.question}`
+        box.appendChild(title)
+
+        question.answers.forEach(answer => {
+            var option
+            if (answer.type == "inp") {
+                option = document.createElement("input")
+                option.type = "text"
+                option.className = answer.type
+                option.placeholder = answer.text
+            } else {
+                option = document.createElement("button")
+                option.textContent = `${answer.text}`
+                option.setAttribute("class", answer.type)
+            }
+            box.appendChild(option)
+        });
+
+        document.body.appendChild(container)
+    });
+}
 
 function showLogsQuestions() {
     var qtdQuestoes = sessionStorage.getItem("questoes")
-    console.log("qtd de questoes" + qtdQuestoes)
     logs.forEach(question => {
         qtdQuestoes++
-        console.log("qtd de questoes" + qtdQuestoes)
+        // console.log("qtd de questoes logs: " + qtdQuestoes)
 
         var container = document.createElement("div")
         container.classList.add("container", "logs")
@@ -257,6 +403,7 @@ function showLogsQuestions() {
 
         document.body.appendChild(container)
     });
+    // sessionStorage.setItem("questoes", qtdQuestoes)
 }
 
 // validação para emails dock.tech
@@ -273,33 +420,30 @@ function documentacao(acesso) {
         document.getElementById('acesso').style.display = "flex"
         document.getElementById('pilarObs').style.display = "flex"
     }
-
 }
 
 function removerLogs() {
-    var logs = document.getElementsByClassName('logs')
-    for (let index = 0; index < logs.length; index++) {
-        logs[index].remove()
-    }
+    var logsArray = Array.from(document.getElementsByClassName('logs'))
+    logsArray.forEach(function(log) {
+        log.remove()
+    })
+}
+
+function removerMetrics() {
+    var metricsArray = Array.from(document.getElementsByClassName('metrics'))
+    metricsArray.forEach(function(metric) {
+        metric.remove()
+    })
 }
 
 function caminhoPilar(pilar) {
+    loadQuestions(5)
     if (pilar == "metrics") {
         removerLogs()
-
-        var metricas = document.getElementsByClassName('metrics')
-
-        for (let i = 0; i < metricas.length; i++) {
-            metricas[i].style.display = "flex"
-        }
+        showMetricsQuestions()
     } else if (pilar == "logs") {
-        var logs = document.getElementsByClassName('logs')
-        console.log("Qtd: " + logs.length)
-
+        removerMetrics()
         showLogsQuestions()
-        // for (let i = 0; i < logs.length; i++) {
-        //     logs[i].style.display = "flex"
-        // }
     } else if (pilar == "traces") {
         document.getElementById('tracesDoc').style.display = "flex"
     }
